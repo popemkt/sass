2b8de50a1884122ac704a5e17c2c3fcffaf70a39
Hoang <popemkt6@gmail.com>
2019-10-17 15:12:45 +0700

    refine

--- test/script.test.js
+++ test/script.test.js
@@ -1,63 +1,92 @@
 
 describe('script', function() {
-  
+
+  var Module;
   beforeEach(function() {
     var fixture = `<div id="modal-container" style="display: none" class="modal-container">
       <div id="video-modal" class="modal">
           <div class="container">
-              <div class="title">Maroon 5 - Memories</div>
-              <video class="video" id="video" autoplay controls allowfullscreen>
+              <video class="video" id="video" controls allowfullscreen>
                   <source src="C:\Frontend\Sass\build\video.mp4" type="video/mp4">
-                  Your browser does not support the video tag.
               </video>
-              <div id="videoCaption">
-                      Music video of maroon 5.
-              </div>
           </div>
           <div id="btn-exit" class="btn-exit">
               <i class="fa fa-times"></i>
           </div>   
       </div>
-      <button id="btn-show-video" class="btn">Click for video</button>
+      <button id="btn-show-video" class="btn"></button>
     </div>`;
 
     document.body.insertAdjacentHTML(
       'afterbegin', 
     fixture);
     
-    videoModal.self.init();
+    Module = videoModal.self;
+
   });
 
 
   afterEach(function() {
     document.body.removeChild(document.getElementById('modal-container'));
+    sinon.restore();
   });
 
-  it('should show and play', function() {
-    let modal = document.getElementById('modal-container');
-    let video = document.getElementById('video');
-    expect(modal).to.not.be.null;
-    document.getElementById('btn-show-video').click();
-    expect(modal.style.display).to.equal('block');
-    setTimeout(() => expect(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2).to.be.true, 300);
-  });
+  describe('init', function() {
+    it('should attach event handler properly', function () {
+      var showStub = sinon.stub(Module, 'show');
+      var hideStub = sinon.stub(Module, 'hide')
+      var btnShow = document.getElementById('btn-show-video');
+      var modalContainer = document.getElementById('modal-container');
 
-  it('should exit by clicking out', function() {
-    let modal = document.getElementById('modal-container');
-    let video = document.getElementById('video');
-    document.getElementById('btn-show-video').click();
-    document.getElementById('modal-container').click();    
-    expect(modal.style.display).to.equal('none');
-    expect(video.paused).to.be.true;
+      Module.init();
+      btnShow.click();
+      modalContainer.click();
+
+      expect(showStub.calledOnce).to.be.true; 
+      expect(hideStub.callCount).to.equal(2); 
+    });
   })
 
-  it('should exit by clicking out', function() {
-    let modal = document.getElementById('modal-container');
-    let video = document.getElementById('video');
-    document.getElementById('btn-show-video').click();
-    document.getElementById('btn-exit').click();    
-    expect(modal.style.display).to.equal('none');
-    expect(video.paused).to.be.true;
+  describe('show', function() {
+    it.only('should show properly', function () {
+      var btnShow = document.getElementById('btn-show-video');
+      var modalContainer = document.getElementById('modal-container');
+      var video = document.getElementById('video');
+
+      Module.init();
+      btnShow.click();
+
+      expect(modalContainer.style.display).to.equal('block'); 
+      setTimeout( () => expect(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2).to.be.true, 300);
+    })
   })
-  
+
+  describe('hide', function() {
+    it('should hide properly clicking button', function() {
+      var btnShow = document.getElementById('btn-show-video');
+      var modalContainer = document.getElementById('modal-container');
+      var btnHide = document.getElementById('btn-exit');
+      var video = document.getElementById('video');
+
+      Module.init();
+      btnShow.click();
+      btnHide.click();
+
+      expect(modalContainer.style.display).to.equal('none'); 
+      expect(video.paused).to.be.true;
+    })
+
+    it('should hide properly clicking out', function() {
+      var btnShow = document.getElementById('btn-show-video');
+      var modalContainer = document.getElementById('modal-container');
+      var video = document.getElementById('video');
+
+      Module.init();
+      btnShow.click();
+      modalContainer.click();
+
+      expect(modalContainer.style.display).to.equal('none'); 
+      expect(video.paused).to.be.true;
+    })
+  })
 });
